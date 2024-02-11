a9fd74e12dbf91c32e737241ca85158bc038820c
JulianH99 <juliancorredor99@gmail.com>
2024-02-11 10:18:12 -0500

    Added views and building dashboard

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("", views.login, name="login")
+    path("login", views.login, name="login"),
+    path("dashboard", views.dashboard, name="dashboard")
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
+from .forms import LoginForm
 # Create your views here.
 
 
@@ -8,4 +10,18 @@ def index(request):
 
 
 def login(request):
-    return render(request, "app/login.html", {})
+
+    if request.method == 'GET':
+        return render(request, "app/login.html", {})
+    else:
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+
+            if data['email'] and data['password']:
+                return HttpResponseRedirect('/dashboard')
+
+
+def dashboard(request):
+    return render(request, "app/dashboard.html", {})
