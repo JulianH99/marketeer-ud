d1f2122c2144b4878dcc964fef9e89a9f215b920
JulianH99 <juliancorredor99@gmail.com>
2024-02-11 17:42:07 -0500

    Added dashboard with charts view

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from random import randint
 
 from .forms import LoginForm
 # Create your views here.
@@ -24,4 +25,23 @@ def login(request):
 
 
 def dashboard(request):
-    return render(request, "app/dashboard.html", {})
+    social_media_accounts_names = [
+        'facebook', 'twitter', 'instagram', 'youtube']
+
+    social_media_alias = {
+        'facebook': 'Test Business',
+        'twitter': '@testBusiness134',
+        'instagram': 'test_business__',
+        'youtube': 'test_buseness'
+
+    }
+
+    social_media_accounts = [{
+        'name': name.capitalize(),
+        'account': social_media_alias[name],
+        'id': name,
+        'followers': randint(100, 500),
+        'icon': "/app/assets/ic-%s.svg" % name
+    } for name in social_media_accounts_names]
+
+    return render(request, "app/dashboard.html", {"accounts": social_media_accounts})
